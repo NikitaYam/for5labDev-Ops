ca623f61146796f27b518dbad1f191d2078bcf6d
Terrapod <nikyam24@gmail.com>
2026-03-28 12:39:34 +0300

    CI pipline

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
@@ -24,18 +23,42 @@ def test_get_existed_user():
     assert response.status_code == 200
     assert response.json() == users[0]
 
+
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': "unknown@mail.com"})
+    assert response.status_code == 404
+
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "Test User",
+        "email": "test@mail.com"
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
+
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    new_user = {
+        "name": "Duplicate User",
+        "email": users[0]['email']
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code == 409
+
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    email = users[1]['email']
+
+    response = client.delete("/api/v1/user", params={'email': email})
+
+    assert response.status_code == 204
